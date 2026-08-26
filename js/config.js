4bcc16d6f465ec2503161c467dcadf1a4a692199
VallejoOsorio2026 <jpvallejoo@eafit.edu.co>
2026-08-26 11:36:19 -0500

    chore: actualizar VERSION_SISTEMA a v1.2.0

--- js/config.js
+++ js/config.js
@@ -23,6 +23,6 @@ const CONFIG = {
 
   // Version logica de la aplicacion. Se registra en cada
   // solicitud para saber que codigo produjo cada respuesta.
-  VERSION_SISTEMA: 'v1.1.0'
+  VERSION_SISTEMA: 'v1.2.0'
 
 };
